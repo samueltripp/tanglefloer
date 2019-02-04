ad8c8c3c9f7acc772f212bc2747cada0541b5bdb
Sam Tripp <samuel.tripp1@gmail.com>
2019-02-04 14:33:37 -0500

    silly final last commit then I think this is all clean and running in sage

--- loadfile.py
+++
@@ -1,5 +0,0 @@
-from AMinus.alg_element import AlgElement
-from AMinus.generator import Generator
-from AMinus.sign_sequence import SignSequence
-
-from sage.combinat.free_module import CombinatorialFreeModule
