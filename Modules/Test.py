1a23bf23c2f03062b3b78421cd55f007090c17e9
Sam Tripp <samuel.tripp1@gmail.com>
2019-07-18 11:01:37 -0400

    afdsfas

--- Modules/Test.py
+++ Modules/Test.py
@@ -8,3 +8,8 @@ under = ETangle(ETangle.Type.UNDER, (-1, 1), 1)
 cap = ETangle(ETangle.Type.CAP, (1, -1), 1)
 unknot = Tangle([cup, cap])
 unknot2 = Tangle([cup, over, under, cap])
+
+
+e1 = ETangle(ETangle.Type.OVER,(1,1,-1,-1),2)
+sd = StrandDiagram(e1,{2:2,3:3,4:4},{0:1,1:0})
+dplus(sd) == {StrandDiagram(e1,{2:2,3:3,4:4},{0:0,1:1}):e1.polyring['U1']}
