e8d61f08994b6c40bd46e73c85082ad13e15cd4c
Sam Tripp <samuel.tripp1@gmail.com>
2019-07-18 15:38:33 -0400

    verbosity tests

--- Modules/Test.py
+++ Modules/Test.py
@@ -3,7 +3,7 @@ from Modules.CTMinus import *
 
 
 # tests for dminus on caps
-cap = ETangle(ETangle.Type.CAP, (1, 1, -1, 1, 1, 1), 3)
+cap = ETangle(ETangle.Type.CAP, (1,-1, -1, 1, 1, 1), 3)
 cup = ETangle(ETangle.Type.CUP, (1, 1, -1, 1, 1, 1), 3)
 over = ETangle(ETangle.Type.OVER, (1, 1, -1, 1, 1, 1), 3)
 under = ETangle(ETangle.Type.UNDER, (1, 1, -1, 1, 1, 1), 3)
@@ -12,7 +12,7 @@ left = [{1: 1, 5: 2}, {3: 2, 5: 3}, {4: 3, 6: 4}, {1: 1, 5: 3}, {0: 0, 1: 4}, {1
 cup_left = [{1: 1, 4: 2}, {3: 2, 4: 3}, {2: 1, 4: 2}, {1: 1, 4: 3}, {0: 0, 1: 4}, {1: 1, 3: 3}]
 
 tests = {cap: left, cup: cup_left, over: left, under: left}
-
+verbose = True
 
 def fill_right(etangle, left_strands):
     out = {}
@@ -27,5 +27,5 @@ for etangle, strands_list in tests.items():
     for strands in strands_list:
         sd = StrandDiagram(etangle, strands, fill_right(etangle, strands))
         print('strand diagram: ' + str(sd))
-        print('dminus: ' + str(dminus(sd)))
+        print('dminus: ' + str(dminus(sd,verbose)))
     print('\n')
