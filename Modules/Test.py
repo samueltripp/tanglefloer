8004b292b71087a1940a9835487ac3389e32bcf1
zwinkele <zwinkele@gmail.com>
2019-07-18 15:13:44 -0400

    Updated tests

--- Modules/Test.py
+++ Modules/Test.py
@@ -7,20 +7,24 @@ cap1 = ETangle(ETangle.Type.CAP, (1, 1, -1, 1, 1, 1), 3)
 cap2 = ETangle(ETangle.Type.CAP, (1, 1, 1, -1, 1, 1), 3)
 
 
-def fill_right(n, left_strands):
-    return {i: i for i in set(range(n)) - set(left_strands.values())}
+def fill_right(etangle, left_strands):
+    out = {}
+    right_points = set(etangle.right_points())
+    for p in set(etangle.middle_points()) - set(left_strands.values()):
+        out[p] = right_points.pop()
+    return out
 
 
 lefts = [{1: 1, 5: 2}, {3: 2, 5: 4}, {4: 4, 6: 5}, {1: 1, 5: 4}, {0: 0, 1: 5}, {1: 1, 3: 4}]
 
 print('cap1')
 for left in lefts:
-    sd = StrandDiagram(cap1, left, fill_right(7, left))
+    sd = StrandDiagram(cap1, left, fill_right(cap1, left))
     print('strand diagram: ' + str(sd))
     print('dminus: ' + str(dminus(sd)))
 
 print('cap2')
 for left in lefts:
-    sd = StrandDiagram(cap2, left, fill_right(7, left))
+    sd = StrandDiagram(cap2, left, fill_right(cap1, left))
     print('strand diagram: ' + str(sd))
     print('dminus: ' + str(dminus(sd)))
