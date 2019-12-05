9b552a560e498820279c1bf544613e209d05a264
Zach <zwinkele@gmail.com>
2019-12-05 17:16:04 -0500

    Updated SignAlgebra tests to use unittest
    - Not really important, but might make adding new tests easier

--- Test/SignAlgebraTests.py
+++ Test/SignAlgebraTests.py
@@ -1,30 +1,39 @@
+import unittest
 from SignAlgebra.AMinus import *
 
-# A few tests
-am = AMinus([-1, 1, 1, -1])
-r = am.polyring
-U1 = am.polyring['U1']
-U2 = am.polyring['U2']
 
-# First multiplication of single elements
-# double crosses black, should be empty product
-elt1 = am.gen({4: 0, 3: 1})
-elt2 = am.gen({0: 4, 1: 3})
-assert elt1 * elt2 == am.zero()
+class TestAMinus(unittest.TestCase):
 
-# double crosses two rightward strands, should be nontrivial product
-elt3 = am.gen({4: 0, 3: 1})
-elt4 = am.gen({0: 0, 1: 3})
-assert elt3 * elt4 == (U1 * U2) * am.gen({4: 0, 3: 3})
+    def test_mul(self):
+        am = AMinus([-1, 1, 1, -1])
+        r = am.polyring
+        U1 = am.polyring['U1']
+        U2 = am.polyring['U2']
 
-# just straightforward multiplication
-elt5 = am.gen({4: 1, 3: 2})
-elt6 = am.gen({2: 1, 1: 0})
-assert elt5 * elt6 == am.gen({4: 0, 3: 1})
+        # First multiplication of single elements
+        # double crosses black, should be empty product
+        elt1 = am.gen({4: 0, 3: 1})
+        elt2 = am.gen({0: 4, 1: 3})
+        assert elt1 * elt2 == am.zero()
 
-# testing differentials
-elt = am.gen({0: 4, 1: 2, 3: 1})
-assert elt.diff() == U1 * am.gen({0: 4, 1: 1, 3: 2}) + am.gen({0: 2, 1: 4, 3: 1})
+        # double crosses two rightward strands, should be nontrivial product
+        elt3 = am.gen({4: 0, 3: 1})
+        elt4 = am.gen({0: 0, 1: 3})
+        assert elt3 * elt4 == (U1 * U2) * am.gen({4: 0, 3: 3})
 
-# testing gradings
-# TODO
+        # just straightforward multiplication
+        elt5 = am.gen({4: 1, 3: 2})
+        elt6 = am.gen({2: 1, 1: 0})
+        assert elt5 * elt6 == am.gen({4: 0, 3: 1})
+
+    def test_diff(self):
+        am = AMinus([-1, 1, 1, -1])
+        r = am.polyring
+        U1 = am.polyring['U1']
+        U2 = am.polyring['U2']
+
+        elt = am.gen({0: 4, 1: 2, 3: 1})
+        assert elt.diff() == U1 * am.gen({0: 4, 1: 1, 3: 2}) + am.gen({0: 2, 1: 4, 3: 1})
+
+    def test_gradings(self):
+        pass  # TODO
